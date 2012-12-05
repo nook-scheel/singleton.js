0582255c3a8c37ce445e4fd793c5b187a76c5f4c
Maxim Sirotenko <nook@live.ru>
2012-12-06 02:50:49 +0400

    Extend test

--- tests/singleton_test.js
+++ tests/singleton_test.js
@@ -1,17 +1,30 @@
 var assert = chai.assert;
 
-describe('Singleton.js', function(){
-  describe('#Singleton()', function(){
+describe('Singleton.js', function () {
+  describe('#Singleton()', function () {
     // Singleton init
     
-    it('should return a preloader object', function() {
+    it('should return a preloader object', function () {
       var singleton = Singleton([1,2]);
       assert.typeOf(singleton, 'object', 'we have an object');
     });
-    it('should return a list array', function() {
+    it('should return a list array', function () {
       var singleton = Singleton([1,2]);
       var list = singleton.list();
       assert.lengthOf(list, 2, 'list value has a length of 2');
+    });
+    it('should extend current singleton object', function() {
+      var singleton = Singleton();
+
+      Singleton.prototype.add = function (value) {
+        this.innerArray.push(value);
+      }
+
+      singleton.add(3);
+      
+      var list = singleton.list();
+
+      assert.lengthOf(list, 3, 'list value has a length of 3');
     });
   });
 });
